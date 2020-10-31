d37eb69411c473fb29839660462c0ac457494bf9
cohix <connorjhicks@gmail.com>
2020-10-31 13:42:11 -0400

    use Cobra

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/pkg/errors v0.9.1
+	github.com/spf13/cobra v1.1.1
 	github.com/suborbital/grav v0.0.11
 	github.com/suborbital/hive v0.1.3
 	github.com/suborbital/hive-wasm v0.1.1
--- main.go
+++ main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
-	"log"
-
+	"github.com/spf13/cobra"
 	"github.com/suborbital/atmo/atmo"
+	"github.com/suborbital/atmo/release"
 )
 
 func main() {
-	server := atmo.New()
+	cmd := rootCommand()
+	cmd.Execute()
+}
 
-	if err := server.Start("./runnables.wasm.zip"); err != nil {
-		log.Fatal(err)
+func rootCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "atmo [bundle-path]",
+		Short: "Atmo function-based web service runner",
+		Long: `
+Atmo is an all-in-one function-based web service platform that enables 
+building backend systems using composable Wasm modules in a declarative manner.
+
+Atmo automatically scales using a meshed message bus, job scheduler, and 
+flexible API gateway to handle any workload. 
+
+Handling API and event-based traffic is made simple using the declarative 
+Directive format and the powerful Runnable API using a variety of languages.`,
+		Version: release.AtmoDotVersion,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			path := "./runnables.wasm.zip"
+			if len(args) > 0 {
+				path = args[0]
+			}
+
+			server := atmo.New()
+
+			return server.Start(path)
+		},
 	}
+
+	cmd.SetVersionTemplate("{{.Version}}\n")
+
+	return cmd
 }
