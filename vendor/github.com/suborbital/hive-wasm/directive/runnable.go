a32a59b844760745263c5feb6296bed061b4f87b
cohix <connorjhicks@gmail.com>
2020-12-31 12:07:05 -0500

    remove vendor

--- vendor/github.com/suborbital/hive-wasm/directive/runnable.go
+++
@@ -1,9 +0,0 @@
-package directive
-
-// Runnable is the structure of a .runnable.yaml file
-type Runnable struct {
-	Name       string `yaml:"name"`
-	Namespace  string `yaml:"namespace"`
-	Lang       string `yaml:"lang"`
-	APIVersion string `yaml:"apiVersion,omitempty"`
-}
