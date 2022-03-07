3bcb4d8fc1dae956e4bdf265505005321f6aa4f2
Gabor Javorszky <gabor@suborbital.dev>
2022-03-07 11:58:56 +0000

    Add options and connection for configuring an exporter for atmo

--- atmo/init_proxy.go
+++ atmo/init_proxy.go
@@ -4,9 +4,9 @@ package atmo
 
 import (
 	"context"
-	"fmt"
 	"time"
 
+	"github.com/pkg/errors"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
@@ -14,7 +14,9 @@ import (
 	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
 	semconv "go.opentelemetry.io/otel/semconv/v1.7.0"
+	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
 
 	"github.com/suborbital/atmo/atmo/options"
 	"github.com/suborbital/vektor/vlog"
@@ -26,44 +28,54 @@ func setupLogger(_ *vlog.Logger) {
 
 // setupTracing configure open telemetry to be used with otel exporter. Returns a tracer closer func and an error.
 func setupTracing(config options.TracerConfig, logger *vlog.Logger) (func(), error) {
-	traceOpts := []trace.TracerProviderOption{
-		trace.WithSampler(trace.TraceIDRatioBased(config.Probability)),
-	}
+	exporterString := "collector"
+	exporterOpts := make([]otlptracegrpc.Option, 0)
 
-	if config.Endpoint != "" && config.APIKey != "" {
-		exporter, err := newExporter(context.Background(), config)
+	if config.APIKey != "" {
+		exporterString = "honeycomb"
+		honeyOpts, err := honeycombExporterOptions(config)
 		if err != nil {
-			return func() {}, fmt.Errorf("creating OTLP trace exporter: %w", err)
+			return func() {}, errors.Wrap(err, "honeycombExporterOptions")
 		}
+		exporterOpts = append(exporterOpts, honeyOpts...)
 
 		logger.Info("created OTLP trace exporter with endpoint and apikey")
-
-		traceOpts = append(traceOpts,
-			trace.WithBatcher(exporter,
-				trace.WithMaxExportBatchSize(trace.DefaultMaxExportBatchSize),
-				trace.WithBatchTimeout(trace.DefaultScheduleDelay*time.Millisecond),
-				trace.WithMaxExportBatchSize(trace.DefaultMaxExportBatchSize),
-			),
-			trace.WithResource(
-				resource.NewWithAttributes(
-					semconv.SchemaURL,
-					semconv.ServiceNameKey.String(config.ServiceName),
-					attribute.String("exporter", "honeycomb"),
-				),
-			),
-		)
 	} else {
-		traceOpts = append(traceOpts,
-			trace.WithResource(
-				resource.NewWithAttributes(
-					semconv.SchemaURL,
-					semconv.ServiceNameKey.String(config.ServiceName),
-					attribute.String("exporter", "collector"),
-				),
-			),
-		)
+		collectorOpts, err := collectorExporterOptions()
+		if err != nil {
+			return func() {}, errors.Wrap(err, "collectorExporterOptions")
+		}
+		exporterOpts = append(exporterOpts, collectorOpts...)
 
 		logger.Info("created tracer configured to use a collector")
+	}
+
+	exporter, err := otlptrace.New(context.Background(), otlptracegrpc.NewClient(exporterOpts...))
+	if err != nil {
+		return func() {}, errors.Wrapf(err, "oltptrace.New with exporter as %s", exporterString)
+	}
+
+	traceOpts := []trace.TracerProviderOption{
+		trace.WithSampler(trace.TraceIDRatioBased(config.Probability)),
+		trace.WithResource(
+			resource.NewWithAttributes(
+				semconv.SchemaURL,
+				semconv.ServiceNameKey.String(config.ServiceName),
+				attribute.String("exporter", exporterString),
+			),
+		),
+		trace.WithBatcher(exporter,
+			trace.WithMaxExportBatchSize(trace.DefaultMaxExportBatchSize),
+			trace.WithBatchTimeout(trace.DefaultScheduleDelay*time.Millisecond),
+			trace.WithMaxExportBatchSize(trace.DefaultMaxExportBatchSize),
+		),
+		trace.WithResource(
+			resource.NewWithAttributes(
+				semconv.SchemaURL,
+				semconv.ServiceNameKey.String(config.ServiceName),
+				attribute.String("exporter", "honeycomb"),
+			),
+		),
 	}
 
 	traceProvider := trace.NewTracerProvider(traceOpts...)
@@ -78,17 +90,25 @@ func setupTracing(config options.TracerConfig, logger *vlog.Logger) (func(), err
 
 }
 
-// newExporter encapsulates putting together the exporter for the tracing. In our case it's an otlptracegrpc client.
-func newExporter(ctx context.Context, config options.TracerConfig) (*otlptrace.Exporter, error) {
-	opts := []otlptracegrpc.Option{
+func collectorExporterOptions() ([]otlptracegrpc.Option, error) {
+	conn, err := grpc.DialContext(context.Background(), "localhost:4317", grpc.WithTransportCredentials(insecure.NewCredentials()), grpc.WithBlock())
+	if err != nil {
+		return nil, errors.Wrap(err, "grpc.DialContext")
+	}
+
+	return []otlptracegrpc.Option{
+		otlptracegrpc.WithGRPCConn(conn),
+		otlptracegrpc.WithTimeout(500 * time.Millisecond),
+	}, nil
+}
+
+func honeycombExporterOptions(config options.TracerConfig) ([]otlptracegrpc.Option, error) {
+	return []otlptracegrpc.Option{
 		otlptracegrpc.WithEndpoint(config.Endpoint),
 		otlptracegrpc.WithHeaders(map[string]string{
 			"x-honeycomb-team":    config.APIKey,
 			"x-honeycomb-dataset": config.Dataset,
 		}),
 		otlptracegrpc.WithTLSCredentials(credentials.NewClientTLSFromCert(nil, "")),
-	}
-
-	client := otlptracegrpc.NewClient(opts...)
-	return otlptrace.New(ctx, client)
+	}, nil
 }
