7ce3cd831b70b4160e097e03ba9c71f9d8c99708
Gabor Javorszky <gabor@suborbital.dev>
2022-11-08 16:05:37 +0000

    Remove unused import

--- sat/engine/wasmtest/assemblyscript_test.go
+++ sat/engine/wasmtest/assemblyscript_test.go
@@ -14,7 +14,6 @@ import (
 	"github.com/suborbital/appspec/request"
 	"github.com/suborbital/e2core/sat/api"
 	"github.com/suborbital/e2core/sat/engine"
-	"github.com/suborbital/e2core/scheduler"
 )
 
 func TestASEcho(t *testing.T) {
