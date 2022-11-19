706dff207dbb4922d40ceef37e40e59466ce3890
Dylan Arbour <arbourd@users.noreply.github.com>
2022-11-19 16:39:46 -0500

    chore: Use built in lint fix
    golangci-lint can fix gci lint errors, and others.

--- sat/ops/gci.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gci write -s Standard -s Default -s "Prefix(github.com/suborbital)" -s "Prefix(github.com/suborbital/e2core/sat)" --NoInlineComments --NoPrefixComments main.go $(find {sat,constd}/ -type f -name '*.go')
