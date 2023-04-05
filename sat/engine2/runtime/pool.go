fdadc3e1d5e8b325bdbcd3f95c63b56b29ec2535
Oscar Spencer <oscar@grain-lang.org>
2023-04-05 09:27:32 -0500

    fix: Use fresh instance on each new plugin invocation

--- sat/engine2/runtime/pool.go
+++ sat/engine2/runtime/pool.go
@@ -66,12 +66,19 @@ func (ip *InstancePool) RemoveInstance() error {
 
 // UseInstance provides an instance from the environment's pool to be used by a callback function
 func (ip *InstancePool) UseInstance(ctx *scheduler.Ctx, instFunc func(*instance.Instance, int32)) error {
-	// grab an instance from the available queue and then
-	// return it to the environment when finished
+	// grab an instance from the available queue
 	inst := <-ip.availableInstances
 
+	go func() {
+		// prepare a new instance
+		if err := ip.AddInstance(); err != nil {
+			panic(err)
+		}
+	}()
+
 	defer func() {
-		ip.availableInstances <- inst
+		inst.Close()
+		inst = nil
 	}()
 
 	// generate a random identifier as a reference to the instance in use to
